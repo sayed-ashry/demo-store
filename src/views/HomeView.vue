56980fca5aa7a6ee951a8caf11a0fcd52cf31c59
sayed <sayedashry1@gmail.com>
2023-12-02 08:22:09 +0200

    create Home View as first screen

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="text-primary">
+    <h1>Home View</h1>
+  </div>
 </template>
+
+<script>
+export default {
+  name: 'HomeView'
+}
+</script>
